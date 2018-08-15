ae189f13073170426fcc825ff9d415a342c0f51f
Feiyang Chen <Eurus_Holmes_999@163.com>
2018-08-15 23:05:16 +0800

    Medium

--- Longest Palindromic Substring.py
+++ Longest Palindromic Substring.py
@@ -13,10 +13,10 @@ class Solution(object):
                     x-=1
                     y+=1
                 return s[x+1:y]
-            s1=ishuiwen(s,i,i)
+            s1=ishuiwen(s,i,i)  # odd
             if len(s1)>len(sub):
                 sub=s1
-            s2=ishuiwen(s,i,i+1)
+            s2=ishuiwen(s,i,i+1)   # even
             if len(s2)>len(sub):
                 sub=s2
         return sub
