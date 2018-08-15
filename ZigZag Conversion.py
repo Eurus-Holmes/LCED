2c9725c6054224d4ca8154c20b34645cd7cf7b49
Feiyang Chen <Eurus_Holmes_999@163.com>
2018-08-16 01:16:55 +0800

    Create ZigZag Conversion.py

---
+++ ZigZag Conversion.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows <= 1 or numRows >= len(s):
+            return s
+        arr = [''] * numRows
+        line, step = 0, -1
+        for c in s:
+            arr[line] += c
+            if line % (numRows-1) == 0:
+                step = - step
+            line += step
+        return ''.join(arr)
