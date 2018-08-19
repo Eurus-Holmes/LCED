91585faa8959bd09630f2f0255858f9c40892029
Feiyang Chen <Eurus_Holmes_999@163.com>
2018-08-19 23:49:59 +0800

    Create Palindrome Number.py

---
+++ Palindrome Number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x<0:
+            return False
+        else:
+            temp=x
+            y=0
+            while temp:
+                y=y*10+temp%10
+                temp=temp/10
+        return x==y
