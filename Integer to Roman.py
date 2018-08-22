15b3d8607e137c549ec5e80f2d10439589b6f529
Feiyang Chen <Eurus_Holmes_999@163.com>
2018-08-22 10:23:48 +0800

    Medium

---
+++ Integer to Roman.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        nums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        strings = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        result = ''
+        for i in range(len(nums)):
+            while num >= nums[i]:
+                num -= nums[i]
+                result += strings[i]
+        return result
+        
