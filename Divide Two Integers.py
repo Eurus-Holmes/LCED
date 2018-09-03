dc976bd628a2c392e31fee2efca1a262ad21dc5b
Feiyang Chen <Eurus_Holmes_999@163.com>
2018-09-04 00:23:40 +0800

    Medium

---
+++ Divide Two Integers.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        MAX_INT = 2147483647
+        sign = 1
+        if dividend >= 0 and divisor < 0 or dividend <= 0 and divisor > 0:
+            sign = -1
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+
+        result = 0
+        current = divisor
+        currentResult = 1
+        while current <= dividend:
+            current <<= 1
+            currentResult <<= 1
+        while divisor <= dividend:
+            current >>= 1
+            currentResult >>= 1
+            if current <= dividend:
+                dividend -= current
+                result += currentResult
+        return min(sign * result, MAX_INT)
+        
