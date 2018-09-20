56051d5ec7e44e5ac6887618e6b2ccea93906d9f
Feiyang Chen <Eurus_Holmes_999@163.com>
2018-09-20 09:46:03 +0800

    Create Jump Game II.py

---
+++ Jump Game II.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        length = len(nums)
+        counter = 0
+        longest = 0
+        reach = 0
+        for i in range(length):
+            if longest < i:
+                counter += 1
+                longest = reach
+            reach = max(reach, nums[i] + i)
+        return counter
