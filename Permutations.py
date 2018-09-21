21f162e8b3c7bd19c31488b26813a7e09e2c69a9
Feiyang Chen <Eurus_Holmes_999@163.com>
2018-09-21 14:29:40 +0800

    Create Permutations.py

---
+++ Permutations.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums)==0:
+            return []
+        if len(nums)==1:
+            return [nums]
+        res=[]
+        for i in range(len(nums)):
+            for j in self.permute(nums[:i]+nums[i+1:]):
+                res.append([nums[i]]+j)
+        return res
