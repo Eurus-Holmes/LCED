3a2daf50a68445aa27917aa6d8c6fb05a742b1ca
Feiyang Chen <Eurus_Holmes_999@163.com>
2018-10-06 17:07:45 +0800

    Easy

---
+++ Climbing Stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==0||n==1||n==2){
+            return n;
+        }
+        int[] dp = new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+}
