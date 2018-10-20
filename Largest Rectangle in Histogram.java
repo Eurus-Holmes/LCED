ee0b35dda283e06f87108b04f23f10603df389ef
Feiyang Chen <chenfeiyang@bjfu.edu.cn>
2018-10-20 21:13:41 +0800

    Hard

---
+++ Largest Rectangle in Histogram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int i = 0;
+        int maxArea = 0;
+        int[] h = new int[heights.length + 1];
+        h = Arrays.copyOf(heights, heights.length + 1);
+        while(i < h.length){
+            if(stack.isEmpty() || h[stack.peek()] <= h[i]){
+                stack.push(i++);
+            }else {
+                int t = stack.pop();
+                maxArea = Math.max(maxArea, h[t] * (stack.isEmpty() ? i : i - stack.peek() - 1));
+            }
+        }
+        return maxArea;
+    }
+}
