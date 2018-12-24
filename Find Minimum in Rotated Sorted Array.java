ebed8aea5bb1b71cd81002faad21d43e39c71da1
Feiyang Chen <chenfeiyang@bjfu.edu.cn>
2018-12-25 00:49:36 +0800

    Medium

---
+++ Find Minimum in Rotated Sorted Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length-1;
+        
+        while(low < high && nums[low] >= nums[high]){
+            int mid = (low+high)/2;
+            
+            if(nums[mid]>nums[high]){
+                low = mid+1;
+            }
+            else{
+                high = mid;
+            }
+        }
+        
+        return nums[low];
+    }
+}
