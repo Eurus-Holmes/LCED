cc51e07d2fc6f666007fee0c70c5daab0e86bf36
Feiyang Chen <chenfeiyang@bjfu.edu.cn>
2019-01-14 00:13:37 +0800

    Easy

---
+++ Rotate Array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
+    }
+    
+    private void reverse(int[] nums, int i, int j){
+        while(i<j){
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j){
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
