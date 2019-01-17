fe34b60c2959102029831330b327c58d2c3d0d98
Feiyang Chen <chenfeiyang@bjfu.edu.cn>
2019-01-17 16:12:56 +0800

    Hard

---
+++ Department Top Three Salaries.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+SELECT
+    d.Name AS 'Department', e1.Name AS 'Employee', e1.Salary
+FROM
+    Employee e1
+        JOIN
+    Department d ON e1.DepartmentId = d.Id
+WHERE
+    3 > (SELECT
+            COUNT(DISTINCT e2.Salary)
+        FROM
+            Employee e2
+        WHERE
+            e2.Salary > e1.Salary
+                AND e1.DepartmentId = e2.DepartmentId
+        )
+;
