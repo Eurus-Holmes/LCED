c1aba48e8077787518dbafd501208d3884c2c39c
Feiyang Chen <chenfeiyang@bjfu.edu.cn>
2019-01-17 15:56:00 +0800

    Medium

---
+++ Department Highest Salary.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+SELECT
+    Department.name AS 'Department',
+    Employee.name AS 'Employee',
+    Salary
+FROM
+    Employee
+        JOIN
+    Department ON Employee.DepartmentId = Department.Id
+WHERE
+    (Employee.DepartmentId , Salary) IN
+    (   SELECT
+            DepartmentId, MAX(Salary)
+        FROM
+            Employee
+        GROUP BY DepartmentId
+	)
+;
