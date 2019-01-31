6f8b96eb86e693b2d183535336799f7e765cfb67
Feiyang Chen <chenfeiyang@bjfu.edu.cn>
2019-01-31 23:49:17 +0800

    Easy

---
+++ Count Primes.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 3:
+            return 0
+        primes = [True] * n
+        primes[0] = primes[1] = False
+        for i in range(2, int(n ** 0.5) + 1):
+            if primes[i]:
+                primes[i * i: n: i] = [False] * len(primes[i * i: n: i])
+        return sum(primes)
