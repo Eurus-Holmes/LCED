d3ab41b21e8fe80feda5606a18e9d73babe078a1
Feiyang Chen <chenfeiyang@bjfu.edu.cn>
2019-02-01 11:23:45 +0800

    Easy

---
+++ Isomorphic Strings.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return len(set(zip(s,t))) == len(set(s)) == len(set(t))
+        
