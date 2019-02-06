5b9e99b7cf79897e04b01cc4e3f550535f750b29
Feiyang Chen <chenfeiyang@bjfu.edu.cn>
2019-02-06 13:26:19 +0800

    Medium

---
+++ Course Schedule II.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+        graph = collections.defaultdict(list)
+        for u, v in prerequisites:
+            graph[u].append(v)
+        # 0 = Unknown, 1 = visiting, 2 = visited
+        visited = [0] * numCourses
+        path = []
+        for i in range(numCourses):
+            if not self.dfs(graph, visited, i, path):
+                return []
+        return path
+    
+    def dfs(self, graph, visited, i, path):
+        if visited[i] == 1: return False
+        if visited[i] == 2: return True
+        visited[i] = 1
+        for j in graph[i]:
+            if not self.dfs(graph, visited, j, path):
+                return False
+        visited[i] = 2
+        path.append(i)
+        return True
+        
