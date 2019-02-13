bbfcda5daff44ca20abe55868dde9142601bf964
Feiyang Chen <chenfeiyang@bjfu.edu.cn>
2019-02-13 23:56:19 +0800

    Easy

---
+++ Contains Duplicate.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(nums) != len(set(nums))
+        
