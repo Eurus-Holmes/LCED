274f082ab0d3a3ecf871d997ec22f28ac27d26ac
Feiyang Chen <chenfeiyang@bjfu.edu.cn>
2019-02-18 16:05:56 +0800

    Medium

---
+++ Contains Duplicate III.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def containsNearbyAlmostDuplicate(self, nums, k, t):
+        """
+        :type nums: List[int]
+        :type k: int
+        :type t: int
+        :rtype: bool
+        """
+        if k < 1 or t < 0:
+            return False
+        numDict = collections.OrderedDict()
+        for x in range(len(nums)):
+            key = nums[x] / max(1, t)
+            for m in (key, key - 1, key + 1):
+                if m in numDict and abs(nums[x] - numDict[m]) <= t:
+                    return True
+            numDict[key] = nums[x]
+            if x >=  k:
+                numDict.popitem(last=False)
+        return False
+        
