69dba0be75526d1aadcb84cd144a8f66deb5273f
Feiyang Chen <chenfeiyang@bjfu.edu.cn>
2019-03-01 23:05:19 +0800

    Hard

---
+++ Number of Digit One.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def countDigitOne(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 0:
+            return 0
+        ones, x = [0], 0
+        while 10 ** x <= 0x7FFFFFFF:
+            ones.append(ones[x] * 10 + 10 ** x)
+            x += 1
+        cnt, size = 0, len(str(n))
+        for digit in str(n):
+            digit = int(digit)
+            size -= 1
+            n -= digit * 10 ** size
+            if digit > 1:
+                cnt += digit * ones[size] + 10 ** size
+            elif digit == 1:
+                cnt += n + ones[size] + 1
+        return cnt
+        
