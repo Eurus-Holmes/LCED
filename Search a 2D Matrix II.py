29a496b84097c82f34d314e25f571404b4096768
Feiyang Chen <chenfeiyang@bjfu.edu.cn>
2019-03-08 15:11:40 +0800

    Medium

---
+++ Search a 2D Matrix II.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix or not matrix[0]:
+            return False
+        
+        rows = len(matrix)
+        cols = len(matrix[0])
+        
+        row, col = 0, cols-1
+        
+        while True:
+            if row < rows and col >= 0:
+                if matrix[row][col] == target:
+                    return True
+                elif matrix[row][col] < target:
+                    row += 1
+                else:
+                    col -= 1
+            else:
+                return False
