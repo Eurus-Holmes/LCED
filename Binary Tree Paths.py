f7c0bf9f993cced7d2f47fae88960c6f09d9b859
Feiyang Chen <chenfeiyang@bjfu.edu.cn>
2019-03-12 17:15:14 +0800

    Easy

---
+++ Binary Tree Paths.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        res, path_list = [], []
+        self.dfs(root, path_list, res)
+        return res
+    
+    
+    def dfs(self, root, path_list, res):
+        if not root:
+            return
+        path_list.append(str(root.val))
+        if not root.left and not root.right:
+            res.append('->'.join(path_list))
+        if root.left:
+            self.dfs(root.left, path_list, res)
+        if root.right:
+            self.dfs(root.right, path_list, res)
+        path_list.pop()
+        
