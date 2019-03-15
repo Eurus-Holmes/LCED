f7f6f85b60b03bdd5ae5d8b68cb8a6e73db97453
Feiyang Chen <chenfeiyang@bjfu.edu.cn>
2019-03-15 14:56:44 +0800

    Easy

---
+++ Ugly Number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num <= 0:
+            return False
+        for i in [2, 3, 5]:
+            while num % i == 0:
+                num = num / i
+        
+        return True if num == 1 else False
+        
