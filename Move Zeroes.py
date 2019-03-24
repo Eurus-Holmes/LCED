82e9b3d90af8489e500d66d39d25348d6c85ae3d
Feiyang Chen <chenfeiyang@bjfu.edu.cn>
2019-03-24 10:51:05 +0800

    Easy

---
+++ Move Zeroes.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        for num in nums:
+            if num != 0:
+                nums[i] = num
+                i += 1
+        for j in xrange(i, len(nums)):
+            nums[j] = 0
+        return nums
+        
+        
