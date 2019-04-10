a5e26f423249c9f55070afa694d9e3e3accc7abd
Feiyang Chen <chenfeiyang@bjfu.edu.cn>
2019-04-10 11:25:25 +0800

    Medium

---
+++ Super Ugly Number.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def nthSuperUglyNumber(self, n, primes):
+        """
+        :type n: int
+        :type primes: List[int]
+        :rtype: int
+        """
+        uglies = [1]
+        def gen(prime):
+            for ugly in uglies:
+                yield ugly * prime
+        merged = heapq.merge(*map(gen, primes))
+        while len(uglies) < n:
+            ugly = next(merged)
+            if ugly != uglies[-1]:
+                uglies.append(ugly)
+        return uglies[-1]
+        
