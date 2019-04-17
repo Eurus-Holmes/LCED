26805f195fce21ac4e701fad63b0bebfd1bde0b5
Feiyang Chen <chenfeiyang@bjfu.edu.cn>
2019-04-17 14:06:48 +0800

    Hard

---
+++ Create Maximum Number.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def maxNumber(self, nums1, nums2, k):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        def getMax(nums, t):
+            ans = []
+            size = len(nums)
+            for x in range(size):
+                while ans and len(ans) + size - x > t and ans[-1] < nums[x]:
+                    ans.pop()
+                if len(ans) < t:
+                    ans += nums[x],
+            return ans
+
+        def merge(nums1, nums2):
+            ans = []
+            while nums1 or nums2:
+                if nums1 > nums2:
+                    ans += nums1[0],
+                    nums1 = nums1[1:]
+                else:
+                    ans += nums2[0],
+                    nums2 = nums2[1:]
+            return ans
+        
+        len1, len2 = len(nums1), len(nums2)
+        res = []
+        for x in range(max(0, k - len2), min(k, len1) + 1):
+            tmp = merge(getMax(nums1, x), getMax(nums2, k - x))
+            res = max(tmp, res)
+        return res
+        
