5c36dc6acdc9efeff60b9b7a0814740ecee1d7ff
Feiyang Chen <chenfeiyang@bjfu.edu.cn>
2019-05-13 16:59:47 +0800

    Hard

---
+++ Max Sum of Rectangle No Larger Than K.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def maxSumSubmatrix(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        m = len(matrix)
+        n = len(matrix[0]) if m else 0
+
+        M = max(m, n)
+        N = min(m, n)
+        ans = None
+        for x in range(N):
+            sums = [0] * M
+            for y in range(x, N):
+                slist, num = [], 0
+                for z in range(M):
+                    sums[z] += matrix[z][y] if m > n else matrix[y][z]
+                    num += sums[z]
+                    if num <= k: ans = max(ans, num)
+                    i = bisect.bisect_left(slist, num - k)
+                    if i != len(slist): ans = max(ans, num - slist[i])
+                    bisect.insort(slist, num)
+        return ans or 0
