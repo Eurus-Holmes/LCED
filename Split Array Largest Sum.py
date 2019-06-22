17efea46f6106efdf520c29db3e1583c3276af0b
Feiyang Chen <feiyangchen98@gmail.com>
2019-06-22 21:49:10 +0800

    Hard

---
+++ Split Array Largest Sum.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def splitArray(self, nums, m):
+        """
+        :type nums: List[int]
+        :type m: int
+        :rtype: int
+        """
+        size = len(nums)
+        high = sum(nums)
+        low = high / m
+        while low <= high:
+            mid = (low + high) / 2
+            n = m
+            cnt = 0
+            valid = True
+            for x in range(size):
+                if nums[x] > mid:
+                    valid = False
+                    break
+                if cnt + nums[x] > mid:
+                    n -= 1
+                    cnt = 0
+                cnt += nums[x]
+                if n == 0:
+                    valid = False
+                    break
+            if valid:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low
