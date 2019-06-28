db200a48dbddc3fd0b52ec60bdc651466f89b556
Feiyang Chen <feiyangchen98@gmail.com>
2019-06-27 21:18:44 -0400

    Easy

---
+++ Add Strings.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        result = []
+        carry = 0
+        idx1, idx2 = len(num1), len(num2)
+        while idx1 or idx2 or carry:
+            digit = carry
+            if idx1:
+                idx1 -= 1
+                digit += int(num1[idx1])
+            if idx2:
+                idx2 -= 1
+                digit += int(num2[idx2])
+            carry = digit > 9
+            result.append(str(digit % 10))
+        return ''.join(result[::-1])
+        
