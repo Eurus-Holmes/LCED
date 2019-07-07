f1cf3113626c33c64f22da69adca1ba58621d61b
Feiyang Chen <feiyangchen98@gmail.com>
2019-07-07 14:13:32 -0400

    Medium

---
+++ Reconstruct Original Digits from English.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def originalDigits(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        cnts = collections.Counter(s)
+        nums = ['six', 'zero', 'two', 'eight', 'seven', 'four', 'five', 'nine', 'one', 'three']
+        numc = [collections.Counter(num) for num in nums]
+        digits = [6, 0, 2, 8, 7, 4, 5, 9, 1, 3]
+        ans = [0] * 10
+        for idx, num in enumerate(nums):
+            cntn = numc[idx]
+            t = min(cnts[c] / cntn[c] for c in cntn)
+            ans[digits[idx]] = t
+            for c in cntn:
+                cnts[c] -= t * cntn[c]
+        return ''.join(str(i) * n for i, n in enumerate(ans))
+        
