f6c484868d7a176ad872b5c207dc353e6e621acd
Feiyang Chen <feiyangchen98@gmail.com>
2019-07-13 11:21:45 -0400

    Easy

---
+++ Find All Anagrams in a String.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        ls, lp = len(s), len(p)
+        cp = collections.Counter(p)
+        cs = collections.Counter()
+        ans = []
+        for i in range(ls):
+            cs[s[i]] += 1
+            if i >= lp:
+                cs[s[i - lp]] -= 1
+                if cs[s[i - lp]] == 0:
+                    del cs[s[i - lp]]
+            if cs == cp:
+                ans.append(i - lp + 1)
+        return ans
+        
