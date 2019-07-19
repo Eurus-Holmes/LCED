951de3997ae8196e7594582048d32141fff0d7b2
Feiyang Chen <feiyangchen98@gmail.com>
2019-07-19 01:00:56 -0400

    Hard

---
+++ Arithmetic Slices II - Subsequence.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def numberOfArithmeticSlices(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        size = len(A)
+        ans = 0
+        dp = [collections.defaultdict(int) for x in range(size)]
+        for x in range(size):
+            for y in range(x):
+                delta = A[x] - A[y]
+                dp[x][delta] += 1
+                if delta in dp[y]:
+                    dp[x][delta] += dp[y][delta]
+                    ans += dp[y][delta]
+        return ans
